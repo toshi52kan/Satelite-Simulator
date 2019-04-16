bf09d016817c16a085438a6ab839cffc8ba9e994
U-DESKTOP-PGG4GLV\toshihiro kanda <kan.toshi52@gmail.com>
2019-04-16 21:42:58 +0900

    add variable.h

---
+++ variable.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef _VARIABLE
+#define _VARIABLE
+#include "matrix.h"
+
+
+matrix_t *I = 0;
+matrix_t *P = 0;
+matrix_t *I_kalman = 0;
+matrix_t *Ad = 0;
+matrix_t *Bd = 0;
+matrix_t *Q = 0;
+matrix_t *C = 0;
+matrix_t *R = 0;
+matrix_t *kalmangain = 0;
+matrix_t *quat_delta = 0;
+matrix_t *STT = 0;
+matrix_t *gyro_bias = 0;
+matrix_t *hoge = 0;
+
+#endif
